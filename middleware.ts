4cfe28c24113850c135679ab7c76c8bbf8b7161d
Berzan Özejder <berzanozejder@gmail.com>
2024-04-08 00:36:00 +0300

    username engine

--- middleware.ts
+++ middleware.ts
@@ -37,6 +37,12 @@ export default auth((req) => {
 	const { nextUrl } = req;
 	const isAuthenticated = !!req.auth;
 
+	if (nextUrl.pathname.includes("@")) {
+		const year = nextUrl.pathname.split("/")[1];
+		const username = nextUrl.pathname.split("/")[2].replace("@", "");
+		return NextResponse.redirect(new URL(`/elections/20${year}/candidates/${username}`, nextUrl.origin));
+	}
+
 	if (nextUrl.pathname === "/login" && isAuthenticated) {
 		return NextResponse.redirect(new URL("/", nextUrl.origin));
 	}
